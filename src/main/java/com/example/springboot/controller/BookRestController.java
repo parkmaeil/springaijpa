810ffdf2edcb279c2c11f97f60c1a27961670255
bitcocom <bitcocom@empas.com>
2024-09-24 12:51:40 +0900

    jpa

--- src/main/java/com/example/springboot/controller/BookRestController.java
+++ src/main/java/com/example/springboot/controller/BookRestController.java
@@ -3,23 +3,37 @@ package com.example.springboot.controller;
 import com.example.springboot.entity.Book;
 import com.example.springboot.entity.BookDTO;
 import com.example.springboot.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Book REST API", description = "책 API 서비스입니다.")
 public class BookRestController {
 
     public final BookService bookService;
     // GET : http://localhost:8081/api/book
     @GetMapping("/book")
+    @Tag(name = "Book REST API")
+    @Operation(summary = "Book 조회", description = "책 조회 서비스입니다.")
     public List<BookDTO> books(){
         List<BookDTO> books=bookService.getAllBooksDTO();
         return books; //JSON(MessageConverter:순환참조문제X)
     }
+
+    @PostMapping("/book")
+    public BookDTO register(@RequestBody BookDTO dto){
+
+        return null;
+    }
+
+
+
+
+
 }
